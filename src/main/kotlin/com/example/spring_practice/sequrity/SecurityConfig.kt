d54d0c03af66e4aed2d39270f7d8c90e701384f3
Priyanshu_raj460 <priyanshu.raj@digivive.com>
2025-04-06 11:40:53 +0530

    mergered from Notes

--- src/main/kotlin/com/example/spring_practice/sequrity/SecurityConfig.kt
+++ src/main/kotlin/com/example/spring_practice/sequrity/SecurityConfig.kt
@@ -1,5 +1,6 @@
 package com.example.spring_practice.sequrity
 
+import jakarta.servlet.DispatcherType
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
@@ -15,6 +16,18 @@ class SecurityConfig {
         return httpSecurity
             .csrf{ csrf -> csrf.disable()}
             .sessionManagement{it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)}
+            .authorizeHttpRequests{ auth ->
+                auth.requestMatchers("/auth/**")
+                    .permitAll()
+                    .dispatcherTypeMatchers(
+                        DispatcherType.ERROR,
+                        DispatcherType.FORWARD
+                    )
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+
+            }
             .build()
 
     }
